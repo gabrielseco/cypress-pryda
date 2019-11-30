9194643aa009d31e906edf572c2f8aa51bed4e9b
Gabriel García Seco <gabriel@streamloots.com>
2019-11-30 20:17:20 +0100

    add express for heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-console */
 const axios = require('axios');
+const express = require('express');
+
 const { sleep, getDateRightNow } = require('./utils');
 const Twitter = require('./twitter');
 require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
 
 const twitter = new Twitter({
   consumerKey: process.env.CONSUMER_KEY,
@@ -13,30 +18,34 @@ const twitter = new Twitter({
 
 function executingFunction({ minutes }) {
   const ms = minutes * 60 * 1000;
-  axios.get('https://kaboodle.co.uk/event/eric-prydz').then(response => {
-  const regex = new RegExp(/Sold Out/i);
-  const matches = response.data.match(/Sold Out/gi).length;
+  axios
+    .get('https://kaboodle.co.uk/event/eric-prydz')
+    .then(response => {
+      const matches = response.data.match(/Sold Out/gi).length;
 
-  if(matches === 3) {
-    console.log("It seems there are not tickets left")
-    console.log(`date executed: `, getDateRightNow());
-    sleep(ms).then(() => executingFunction({ minutes }));
-  }
+      if (matches === 3) {
+        console.log('It seems there are not tickets left');
+        console.log(`date executed: `, getDateRightNow());
+        sleep(ms).then(() => executingFunction({ minutes }));
+      }
 
-  if(matches !== 3) {
-    console.log("Probably tickets")
-    twitter
+      if (matches !== 3) {
+        console.log('Probably tickets');
+        twitter
           .sendTweet(
             `Hey @GGarciaSeco10,\nI have news for you it seems like there are tickets to see Eric Prydz in Printworks.\nCheck this out ➡️ https://kaboodle.co.uk/event/eric-prydz`
           )
           .then(() => console.log('Tweet sent'))
           .catch(err => console.error('Error sending tweet of error OMG', err));
-  }
-
-}).catch(err => {
-  console.log('err axios get', err);
-})
+      }
+    })
+    .catch(err => {
+      console.log('err axios get', err);
+    });
 }
 
-const minutes = 10;
-executingFunction({ minutes });
+app.listen(port, () => {
+  console.log('Listening in port: ', port);
+  const minutes = 10;
+  executingFunction({ minutes });
+});
