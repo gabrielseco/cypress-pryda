261c3fa5c2222cc57b169c08d61d72a92e67ba3f
Gabriel García Seco <gabriel@streamloots.com>
2019-11-30 17:59:13 +0100

    listen to server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-console */
-const cypress = require('cypress')
+const cypress = require('cypress');
+const express = require('express');
+
 const { execCommand, sleep, getDateRightNow } = require('./utils');
 const Twitter = require('./twitter');
-
 require('dotenv').config();
 
+const app = express();
+const port = Number(process.env.PORT || 8080);
 
 const twitter = new Twitter({
   consumerKey: process.env.CONSUMER_KEY,
@@ -15,38 +18,35 @@ const twitter = new Twitter({
 
 function executingFunction({ minutes }) {
   const ms = minutes * 60 * 1000;
-  cypress.run({
-    spec: 'cypress/integration/pryda.spec.js'
-  }).then(results => {
-    if(results.totalFailed > 0) {
-      twitter.sendTweet(
-          `Hey @GGarciaSeco10,\nI have news for you it seems like there are tickets to see Eric Prydz in Printworks.\nCheck this out ➡️ https://kaboodle.co.uk/event/eric-prydz`
-        )
-        .then(() => console.log('Tweet sent'))
-        .catch(err => console.error('Error sending tweet of error OMG', err));
-    }
+  cypress
+    .run({
+      spec: 'cypress/integration/pryda.spec.js'
+    })
+    .then(results => {
+      if (results.totalFailed > 0) {
+        twitter
+          .sendTweet(
+            `Hey @GGarciaSeco10,\nI have news for you it seems like there are tickets to see Eric Prydz in Printworks.\nCheck this out ➡️ https://kaboodle.co.uk/event/eric-prydz`
+          )
+          .then(() => console.log('Tweet sent'))
+          .catch(err => console.error('Error sending tweet of error OMG', err));
+      }
 
-    if(results.totalPassed === results.totalTests) {
-      console.log(`date executed: `, getDateRightNow());
-      sleep(ms).then(() => executingFunction({ minutes }));
-    }
-  }).catch(err => {
-    console.log('err cypress run', err);
-  })
-}
-
-console.log("Hey heroku let's do something fun")
-
-sleep(1000).then(() => {
-  execCommand('cypress install')
-    .then(({ stdout }) => {
-      console.log(stdout)
-      const minutes = 10;
-      executingFunction({ minutes });
+      if (results.totalPassed === results.totalTests) {
+        console.log(`date executed: `, getDateRightNow());
+        sleep(ms).then(() => executingFunction({ minutes }));
+      }
     })
     .catch(err => {
-      console.log("Error installing")
-    })
-})
+      console.log('err cypress run', err);
+    });
+}
 
-
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+  execCommand('cypress install').then(({ stdout }) => {
+    console.log('stdout', stdout);
+    const minutes = 1;
+    executingFunction({ minutes });
+  });
+});
