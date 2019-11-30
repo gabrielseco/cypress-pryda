4772aca04b7a6d08241c5c817768676ea963d2b9
Gabriel García Seco <gabriel@streamloots.com>
2019-11-30 20:09:42 +0100

    replace cypress by axios && regex

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
-const cypress = require('cypress');
-
+const axios = require('axios');
 const { sleep, getDateRightNow } = require('./utils');
 const Twitter = require('./twitter');
 require('dotenv').config();
@@ -14,28 +13,29 @@ const twitter = new Twitter({
 
 function executingFunction({ minutes }) {
   const ms = minutes * 60 * 1000;
-  cypress
-    .run({
-      spec: 'cypress/integration/pryda.spec.js'
-    })
-    .then(results => {
-      if (results.totalFailed > 0) {
-        twitter
+  axios.get('https://kaboodle.co.uk/event/eric-prydz').then(response => {
+  const regex = new RegExp(/Sold Out/i);
+  const matches = response.data.match(/Sold Out/gi).length;
+
+  if(matches === 3) {
+    console.log("It seems there are not tickets left")
+    console.log(`date executed: `, getDateRightNow());
+    sleep(ms).then(() => executingFunction({ minutes }));
+  }
+
+  if(matches !== 3) {
+    console.log("Probably tickets")
+    twitter
           .sendTweet(
             `Hey @GGarciaSeco10,\nI have news for you it seems like there are tickets to see Eric Prydz in Printworks.\nCheck this out ➡️ https://kaboodle.co.uk/event/eric-prydz`
           )
           .then(() => console.log('Tweet sent'))
           .catch(err => console.error('Error sending tweet of error OMG', err));
-      }
+  }
 
-      if (results.totalPassed === results.totalTests) {
-        console.log(`date executed: `, getDateRightNow());
-        sleep(ms).then(() => executingFunction({ minutes }));
-      }
-    })
-    .catch(err => {
-      console.log('err cypress run', err);
-    });
+}).catch(err => {
+  console.log('err axios get', err);
+})
 }
 
 const minutes = 10;
