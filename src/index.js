a92e4ac60b77a69f5d413c25f8810f5f1269a4a8
Gabriel García Seco <gabriel@streamloots.com>
2019-12-03 00:14:46 +0100

    puppeteer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const axios = require('axios');
 const express = require('express');
+const puppeteer = require('puppeteer');
 
 const { sleep, getDateRightNow } = require('./utils');
 const Twitter = require('./twitter');
@@ -16,31 +16,50 @@ const twitter = new Twitter({
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
 });
 
+async function crawl(url) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector('.ticket-selector', { timeout: 3000 });
+
+    const body = await page.evaluate(() => {
+      return document.querySelector('body').innerHTML;
+    });
+    return body;
+
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function executingFunction({ minutes }) {
   const ms = minutes * 60 * 1000;
-  axios
-    .get('https://kaboodle.co.uk/event/eric-prydz')
+  crawl(
+    'https://bookings.kaboodle.co.uk/landing?client_id=68&agent_id=408740&package_id=9924&adults=1&children=0&infants=0&currency_id=98'
+  )
     .then(response => {
-      const matches = response.data.match(/Sold Out/gi).length;
+      const matches = response.match(/fa fa-plus/gm);
 
-      if (matches === 3) {
+      if (matches === null) {
         console.log('It seems there are not tickets left');
         console.log(`date executed: `, getDateRightNow());
         sleep(ms).then(() => executingFunction({ minutes }));
       }
 
-      if (matches !== 3) {
+      if (matches !== null) {
         console.log('Probably tickets');
         twitter
           .sendTweet(
-            `Hey @GGarciaSeco10,\nI have news for you it seems like there are tickets to see Eric Prydz in Printworks.\nCheck this out ➡️ https://kaboodle.co.uk/event/eric-prydz`
+            `Hey @GGarciaSeco10,\nI have news for you it seems like there are tickets to see Eric Prydz in Printworks.\nCheck this out ➡️ https://bookings.kaboodle.co.uk/book/9924/ticket`
           )
           .then(() => console.log('Tweet sent'))
           .catch(err => console.error('Error sending tweet of error OMG', err));
       }
     })
     .catch(err => {
-      console.log('err axios get', err);
+      console.log('err puppeteer', err);
     });
 }
 
